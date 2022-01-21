0929126bc22daa784f21bcdb1cda1d0c3f5ac421
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-21 19:31:36 +0530

    Create Maping.jsx

---
+++ src/components/map/Maping.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Map from "./Maping"
+
+
+export default function Maping() {
+  return (  
+    <>
+    <div id='App'>
+      <Map></Map>
+    </div>
+    </>
+  )
+}
