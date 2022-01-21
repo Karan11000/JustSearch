dd98a655207d4cd88c96465d96435ac4cb4f6eb0
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-21 19:31:05 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="text-center p-10 mt-0.4 border-t dark:border-gray-700  border-gray-200">
+            © 2021 JustSearch, Inc.
+        </div>
+    )
+}
