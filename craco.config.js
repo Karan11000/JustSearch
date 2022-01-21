7a502aeef8f4f7f3f6b64c2c7e9ffcc01eff80d1
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-21 19:30:15 +0530

    Create craco.config.js

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  babel: {
+    loaderOptions: {
+      ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
+    },
+  },
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  }
