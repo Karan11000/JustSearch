6e626fd5d08d21f776e5aad7436a4f633c8e3738
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-29 20:23:08 +0530

    Update File.jsx

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,5 +1,3 @@
-// import { faFile } from "@fortawesome/free-solid-svg-icons"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React from "react"
 
 export default function File({ file }) {
