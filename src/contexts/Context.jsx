dd903f8b602d40462cc410c57e2b0ab358dab264
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-01-29 20:23:23 +0530

    Update Context.jsx

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -13,7 +13,6 @@ function Context({children}) {
     
     const getResults = async (url) => {
         setLoading(true);
-        //api key = 15c72c0c93msh6bb36646e712ea6p1fad69jsn3c67de78c642
      console.log(`${baseUrl}${url}`)
         const res = await fetch(`${baseUrl}${url}`, {
             "method": "GET",
@@ -21,7 +20,7 @@ function Context({children}) {
                 "x-user-agent": "desktop",
 		"x-proxy-location": "US",
 		"x-rapidapi-host": "google-search3.p.rapidapi.com",
-		"x-rapidapi-key": "15c72c0c93msh6bb36646e712ea6p1fad69jsn3c67de78c642"
+		"x-rapidapi-key": "31e62558cfmsha93967ca03ea7bcp16e078jsn87486868290d"
             }
         })
         
